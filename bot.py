3ea4ed5908d5435f3524febe44e562775b9f7433
AnTubeYT <52192335+AnTubeYT@users.noreply.github.com>
2020-07-08 06:38:17 +0300

    Первая версия файлов

---
+++ bot.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+#Импорт библиотек
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+import settings
+import functions
+
+#Добавление кнопок
+functions.initk()
+functions.greenk('зеленая кнопка')
+functions.newlinek()
+functions.bluek('синяя кнопка')
+
+#Подключение к вк
+token = settings.token
+vk = vk_api.VkApi(token=token)
+longpoll = VkLongPoll(vk)
+vk = vk.get_api()
+
+#Отправка сообщений
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        try:
+            if event.to_me:
+                m = event.text
+                m = m.lower()
+                if m == "привет":
+                    msg(event.user_id, 'Привет, как дела?')
+                elif m == "хорошо":
+                    msg(event.user_id, 'Круто, и у меня!')
+                if m == 'кнопки':
+                    msgkbrd(event.user_id, 'держи кнопочки')
+        except:
+            print('ПРОИЗОШЛА ОШИБКА!!!')
---
+++ functions.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+#Импорт библиотек
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+
+#Упрощение команды для сообщения
+def msg(ui,message):
+    vk.messages.send(
+        peer_id=ui,
+        random_id=get_random_id(),
+        message=message
+    )
+#Упрощение команды для сообщения с клавиатурой(кнопками)
+def msgkbrd(ui,message):
+    global keyboard
+    vk.messages.send(
+        peer_id=ui,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=message
+    )
+
+#Функции для клавиатуры
+def initk(): #создание клавиатуры
+    global keyboard
+    keyboard = VkKeyboard(one_time=False)
+    
+def greenk(t): #зелёная кнопка
+    keyboard.add_button(t, color=VkKeyboardColor.POSITIVE)
+
+def redk(t): #красная кнопка
+    keyboard.add_button(t, color=VkKeyboardColor.NEGATIVE)
+
+def bluek(t): #синяя кнопка
+    keyboard.add_button(t, color=VkKeyboardColor.PRIMARY)
+
+def whitek(t): #белая кнопка
+    keyboard.add_button(t, color=VkKeyboardColor.DEFAULT)
+
+def newlinek(): #новая строка
+    keyboard.add_line()
